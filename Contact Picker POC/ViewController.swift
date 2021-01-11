a1ecf6c7f56db6a9f8e7969486b9f5c3c3bc936a
Ramy Sabry <ramy.sabri@venturous.solutions>
2021-01-11 14:14:08 +0200

    Initial Commit

---
+++ Contact Picker POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact Picker POC
+//
+//  Created by Ramy Sabry on 11/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
